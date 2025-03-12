875d7f3920029be8e82ddecea5b6deb519b39cfa
Raymond Wong <raymondwong1701@gmail.com>
2025-03-12 12:08:57 -0700

    initial upload

---
+++ src/main.cpp
@@ -0,0 +1,475 @@
+#include "main.h"
+
+void setup() {
+  try {
+    Serial.begin(115200);
+
+    espNowSetup();
+    sendStatusUpdate(STATUS_STARTING_COMMS_SETUP);
+    sendStatusUpdate(STATUS_FINISHED_PARTIAL_SETUP); // lol
+
+    // setup seven segment display
+    try {
+      sendStatusUpdate(STATUS_STARTING_SSEG_SETUP);
+      sevenSegment.begin(PIN_CLK, PIN_DIO, 6);
+      sevenSegment.setBrightness(4);
+      sendStatusUpdate(STATUS_FINISHED_PARTIAL_SETUP);
+    } catch (...) {
+      throw STATUS_SETUP_ERROR;
+    }
+
+    // init I2C signal with MPU6050
+    try {
+      sendStatusUpdate(STATUS_STARTING_I2C_SETUP);
+      Wire.setPins(PIN_SDA, PIN_SCL);
+      bool success = Wire.begin();
+      if (success) {
+        sendStatusUpdate(STATUS_FINISHED_PARTIAL_SETUP);
+      } else {
+        sendStatusUpdate(STATUS_SETUP_ERROR);
+        return;
+      }
+    } catch (...) {
+      throw STATUS_SETUP_ERROR;
+    }
+
+    // these should need status updates? I hope?
+    pinMode(MPU1, OUTPUT); // mpu 1 (main)
+    pinMode(MPU2, OUTPUT); // mpu 2
+    pinMode(MPU3, OUTPUT); // mpu 3
+
+    Serial.println("Ensure the MPU is held still during calibration...");
+    for (int i = 1; i <= 3; i++) {
+      try {
+        sendStatusUpdate( // send an update about our setup progress
+          (i == 1) ? STATUS_STARTING_MPU1_CALIBRATION :
+          (i == 2) ? STATUS_STARTING_MPU2_CALIBRATION :
+          (i == 3) ? STATUS_STARTING_MPU3_CALIBRATION :
+          STATUS_UNKNOWN_ERROR
+        );
+        mpuSetup(i);
+        sendStatusUpdate(STATUS_FINISHED_PARTIAL_SETUP);
+        delay(50);
+      } catch (...) {
+        throw STATUS_SETUP_ERROR; // handled by outer catch
+      }
+    }
+
+    sendStatusUpdate(STATUS_FINISHED_ALL_SETUP);
+    delay(50);
+    setupFinished = true;
+    yawOffset = -999;
+  } catch (char errorCode) {
+    sendStatusUpdate(errorCode);
+    setupFinished = false;
+  }
+}
+
+void loop() {
+  if (!setupFinished) {
+    return;
+  }
+
+  currentMs = millis();
+  if (currentMs - previousMs < LOOP_TIME) {
+    return;
+  }
+
+  try {
+    for (int i = 1; i <= 3; i++) {
+      setActiveMpu(i);
+      mpuCalc();
+      try {
+        pitches[i-1] = IMU->madgwickFilter.getRoll(); // not sure why, but pitch and roll are swapped
+        rolls[i-1] = IMU->madgwickFilter.getPitch();
+        yaws[i-1] = IMU->madgwickFilter.getYaw();
+
+        pitches[i-1] = -pitches[i-1] + 90; // normalizes pitch between -90 (down) and +90 (up), 0 = level
+        if (isnan(pitches[i-1]) || isnan(rolls[i-1]) || isnan(yaws[i-1])) throw STATUS_CALC_ERROR;
+      } catch (...) {
+        throw STATUS_CALC_ERROR;
+      }
+    }
+  } catch (char errorCode) {
+    sendStatusUpdate(errorCode);
+  }
+
+  // sorts all rotations, so min = 0, mid = 1, max = 2
+  sortFloats(&pitches[0]);
+  sortFloats(&rolls[0]);
+  sortFloats(&yaws[0]);
+
+  // idk why this fails outside of sendPacket() but it does :(
+  // totalDeviations = countDeviation(pitches) + countDeviation(rolls); // yaw deviations not considered
+
+  sendPacket(pitches[1], rolls[1], (yaws[0] + yaws[1] + yaws[2]) / 3);
+
+  printoutCounter++;
+  if (printoutCounter % PRINTOUT_COOLDOWN == 0) {
+    printData();
+  }
+
+  try {
+    updateDisplay(pitches[1], rolls[1]);
+  } catch (...) {
+    sendStatusUpdate(STATUS_SSEG_DISPLAY_ERROR);
+  }
+
+  previousMs = currentMs;
+}
+
+// Returns the number of HIGH deviations (see DEVIATION_THRESHOLD)
+int countDeviation(float* values) {
+  char deviationCount = 0;
+  for (int i = 0; i < 2; i++) {
+    if (abs(values[i] - values[i+1]) > DEVIATION_THRESHOLD) {
+      deviationCount++;
+    }
+  }
+  return deviationCount;
+}
+
+void sendPacket(int pitch, int roll, int yaw) {
+  if (yawOffset == -999) {
+    yawOffset = yaw;
+  }
+  yaw -= yawOffset;
+
+  char status = STATUS_OK;
+  totalDeviations = countDeviation(pitches);// + countDeviation(rolls); // yaw deviations not considered
+  if (totalDeviations >= 2) {
+    status = STATUS_WARNING_EXTREME_DEVIATION;
+  } else if (totalDeviations == 1) {
+    status = STATUS_WARNING_HIGH_DEVIATION;
+  } else {
+    // print status OK if necessary
+  }
+
+  if (yaw < 0) status = status | 0x80; // use MSB of status byte as negative bit for yaw
+
+  // prep packet to be sent
+  packet.status = status;
+  packet.pitch = pitch; // -90 -> 90
+  packet.roll = roll; // -90 -> 90
+  packet.yaw = abs(yaw); // -180 -> 180, negative bit is included as MSB of status byte
+  // send packet over ESP-NOW
+  esp_err_t result = esp_now_send(receiverAddress, (uint8_t*)&packet, sizeof(packet));
+  if (result == ESP_OK) {
+    // Serial.println("Rotation packet sent!");
+  }
+}
+
+void sendStatusUpdate(int status) {
+  packet.status = (char)status;
+  packet.pitch = 0;
+  packet.roll = 0;
+  packet.yaw = 0;
+  // send packet over ESP-NOW
+  esp_err_t result = esp_now_send(receiverAddress, (uint8_t*)&packet, sizeof(packet));
+  if (result == ESP_OK) {
+    Serial.print("Status packet sent:");
+    Serial.println(status);
+  }
+}
+
+// ESP-NOW connection setup
+void espNowSetup() {
+  WiFi.mode(WIFI_STA); // Set device as a Wi-Fi Station
+
+  if (esp_now_init() != ESP_OK) {// Init ESP-NOW
+      Serial.println("Error initializing ESP-NOW");
+      // sendStatusUpdate(-1); // this literally WOULD NOT send???
+      return;
+  }
+
+  memcpy(espReceiver.peer_addr, receiverAddress, 6); // Register peer
+  espReceiver.channel = 0;
+  espReceiver.encrypt = false;
+
+  if (esp_now_add_peer(&espReceiver) != ESP_OK) {
+    Serial.println("Failed to add receiver, make sure it's turned on!");
+    sendStatusUpdate(-1);
+  } else {
+    Serial.println("ESP-NOW setup completed successfully!");
+    sendStatusUpdate(0);
+  }
+}
+
+void mpuSetup(int mpuNum) {
+  if (mpuNum < 1 || mpuNum > 3) {
+    Serial.println("Invalid MPU number");
+  } else {
+    setActiveMpu(mpuNum);
+  }
+  Serial.print("Starting setup for MPU_");
+  Serial.println(mpuNum);
+
+  Wire.begin();
+  delay(100);  
+  transmit(PWR_MGMT_1, 0x00, true);
+  transmit(ACCEL_CONFIG, ACCEL_SENSITIVITY << 3, true); // Specifying output scaling of accelerometer
+  transmit(GYRO_CONFIG, GYRO_SENSITIVITY << 3, true); // Specifying output scaling of gyroscope
+  delay(100);
+
+  Serial.println("Starting Madgwick filter sampler for this MPU...");
+  IMU->madgwickFilter.begin(MADGWICK_FREQ);
+  delay(100);
+
+  // Gyroscope calibration
+  for (int i = 0; i < 1024; i++) {
+    mpuCalc();
+    IMU->gyroOffset.z += IMU->gyroRaw.z;
+    IMU->gyroOffset.y += IMU->gyroRaw.y;
+    IMU->gyroOffset.x += IMU->gyroRaw.x;
+    playLoadingAnimation();
+    delay(3);
+  }
+  IMU->gyroOffset.z = (IMU->gyroOffset.z >> 10); // GyZ_offset = GyZ_offset_sum >> 10;
+  IMU->gyroOffset.y = (IMU->gyroOffset.y >> 10); // GyY_offset = GyY_offset_sum >> 10;
+  IMU->gyroOffset.x = (IMU->gyroOffset.x >> 10); // GyX_offset = GyX_offset_sum >> 10;
+  Serial.print("GyroZ offset value = "); Serial.println(IMU->gyroOffset.z);
+  Serial.print("GyroY offset value = "); Serial.println(IMU->gyroOffset.y);
+  Serial.print("GyroX offset value = "); Serial.println(IMU->gyroOffset.x);
+
+  // Accelerometer calibration
+  float xSum = 0;
+  float ySum = 0;
+  float zSum = 0;
+  int xOffset = round(IMU->accel.x);
+  int yOffset = round(IMU->accel.y);
+  int zOffset = round(IMU->accel.z);
+  for (int i = 0; i < 1024; i++) {
+    mpuCalc();
+    xSum += (IMU->accel.x - xOffset);
+    ySum += (IMU->accel.y - yOffset);
+    zSum += (IMU->accel.z - zOffset);
+    playLoadingAnimation();
+    delay(3);
+  }
+  IMU->accelOffset.x = (xSum / 1024);
+  IMU->accelOffset.y = (ySum / 1024);
+  IMU->accelOffset.z = (zSum / 1024);
+  Serial.print("AccelX offset value = "); Serial.println(IMU->accelOffset.x);
+  Serial.print("AccelY offset value = "); Serial.println(IMU->accelOffset.y);
+  Serial.print("AccelZ offset value = "); Serial.println(IMU->accelOffset.z);
+
+  Serial.print("Finished setup for MPU_");
+  Serial.println(mpuNum);
+}
+
+// Runs all necessary IMU calculations on the active IMU
+void mpuCalc() {
+  try {
+    // Request gyroscope data
+    transmit(0x43, false); // 0x43 is the address of the first gyro register to read (x)
+    Wire.requestFrom(ADDRESS, 6, true);  
+    IMU->gyroRaw.x = Wire.read() << 8 | Wire.read(); // 0x43, 0x44 (GYRO_XOUT_H, GYRO_XOUT_L)
+    IMU->gyroRaw.y = Wire.read() << 8 | Wire.read(); // 0x45, 0x46 (GYRO_YOUT_H, GYRO_YOUT_L)
+    IMU->gyroRaw.z = Wire.read() << 8 | Wire.read(); // 0x47, 0x48 (GYRO_ZOUT_H, GYRO_ZOUT_L)
+
+    // Request accelerometer data
+    transmit(0x3B, false);
+    Wire.requestFrom(ADDRESS, 6, true); 
+    IMU->accel.x = (Wire.read() << 8 | Wire.read()) / 16384.0 - IMU->accelOffset.x; // 0x3B, 0x3C (ACCEL_XOUT_H, ACCEL_XOUT_L)
+    IMU->accel.y = (Wire.read() << 8 | Wire.read()) / 16384.0 - IMU->accelOffset.y; // 0x3D, 0x3E (ACCEL_YOUT_H, ACCEL_YOUT_L)
+    IMU->accel.z = (Wire.read() << 8 | Wire.read()) / 16384.0 - IMU->accelOffset.z; // 0x3F, 0x40 (ACCEL_ZOUT_H, ACCEL_ZOUT_L)
+  } catch (...) {
+    throw STATUS_I2C_COMMS_ERROR;
+  }
+
+  try {
+    // fix accelerometer negatives
+    IMU->accel.x -= (IMU->accel.x > 2) ? 4 : 0;
+    IMU->accel.y -= (IMU->accel.y > 2) ? 4 : 0;
+    IMU->accel.z -= (IMU->accel.z > 2) ? 4 : 0;
+
+    // add mpu6050 offset values
+    IMU->gyroRaw.x -= IMU->gyroOffset.x;
+    IMU->gyroRaw.y -= IMU->gyroOffset.y;
+    IMU->gyroRaw.z -= IMU->gyroOffset.z;
+
+    IMU->gyro.x = IMU->gyroRaw.x / 131;
+    IMU->gyro.y = IMU->gyroRaw.y / 131;
+    IMU->gyro.z = IMU->gyroRaw.z / 131;
+
+    // gyro-based yaw pitch & roll
+    // IMU->gyroAngle.x += IMU->gyroRaw.x * LOOP_TIME / 1000 / 65.536;
+    // IMU->gyroAngle.y += IMU->gyroRaw.y * LOOP_TIME / 1000 / 65.536;
+    // IMU->gyroAngle.z += IMU->gyroRaw.z * LOOP_TIME / 1000 / 65.536;
+
+    // accelerometer-based pitch & roll
+    // IMU->accelAngle.x = vectorToAngle(IMU->accel.x, IMU->accel.y, IMU->accel.z);
+    // IMU->accelAngle.y = vectorToAngle(IMU->accel.y, IMU->accel.x, IMU->accel.z);
+
+    IMU->madgwickFilter.updateIMU(
+      IMU->gyro.x,  IMU->gyro.y,  IMU->gyro.z, // gyro must be in deg/sec
+      IMU->accel.x, IMU->accel.y, IMU->accel.z // accel must be in
+    );
+  } catch (...) {
+    throw STATUS_CALC_ERROR;
+  }
+}
+
+void updateDisplay(int pitch, int roll) {
+  char pitchString[20];
+  char rollString[20];
+  char displayString[20];
+
+  if (pitch < 0) { // negative
+    if (abs(pitch) >= 10) {
+      sprintf(pitchString, "%i", pitch);
+    } else {
+      sprintf(pitchString, " %i", pitch);
+    }
+  } else { // positive
+    if (abs(pitch) >= 10) {
+      sprintf(pitchString, " %i", pitch);
+    } else {
+      sprintf(pitchString, "  %i", pitch);
+    }
+  }
+
+  if (roll < 0) { // negative
+    if (abs(roll) >= 10) {
+      sprintf(rollString, "%i", roll);
+    } else {
+      sprintf(rollString, " %i", roll);
+    }
+  } else { // positive
+    if (abs(roll) >= 10) {
+      sprintf(rollString, " %i", roll);
+    } else {
+      sprintf(rollString, "  %i", roll);
+    }
+  }
+
+  sprintf(displayString, "%s%s", pitchString, rollString);
+  sevenSegment.displayPChar(displayString);
+}
+
+// Used when running accelerometer-based angle calculations. Value is returned in degrees.
+float vectorToAngle(float a, float b, float c) {
+  return atan(a / sqrt( b*b + c*c )) * RAD2DEG;
+  // RAD2DEG is necessary since atan returns a radian (all my homies hate radians).
+}
+
+// Transmits the given data to the active IMU
+void transmit(char address, char value, bool sendStop) {
+  Wire.beginTransmission(ADDRESS);
+  Wire.write(address);
+  Wire.write(value);
+  Wire.endTransmission(sendStop);
+}
+
+// Transmits the given data to the active IMU
+void transmit(char data, bool sendStop) {
+  Wire.beginTransmission(ADDRESS);
+  Wire.write(data);
+  Wire.endTransmission(sendStop);
+}
+
+// updates active MPU ("IMU") and performs the necessary hardware outputs
+void setActiveMpu(int mpuNum) {
+  digitalWrite(MPU1, (mpuNum == 1) ? HIGH : LOW);
+  digitalWrite(MPU2, (mpuNum == 2) ? HIGH : LOW);
+  digitalWrite(MPU3, (mpuNum == 3) ? HIGH : LOW);
+  IMU = (
+    mpuNum == 1 ? &mpu1 :
+    mpuNum == 2 ? &mpu2 :
+    mpuNum == 3 ? &mpu3 :
+    NULL // invalid MPU number
+  );
+  if (IMU == NULL) {
+    throw STATUS_UNKNOWN_ERROR;
+  }
+}
+
+// Used to sort rotations, uses compare-and-swap method
+void sortFloats(float* arr) {
+    for (int i = 0; i < 2; i++) { // Only need to iterate 2 times (akin to comparing i to i+1)
+        for (int j = 0; j < 2 - i; j++) {
+            if (arr[j] > arr[j + 1]) { // Swap the elements
+                float temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+
+bool headerPrinted = false;
+void printData() {
+
+  if (!headerPrinted) {
+    // Serial.println("time,pitch 1,pitch 2,pitch 3,median,error count");
+    Serial.println("time,roll 1,roll 2,roll 3,median,error count");
+    headerPrinted = true;
+  }
+
+  /* * * * * TIME PRINTOUT * * * * */
+  // Serial.print("[");
+  // Serial.print(currentMs - previousMs);
+  // Serial.print(" ms] ");
+
+  Serial.print("");
+  Serial.print((currentMs - previousMs) * printoutCounter / 1000.0);
+  Serial.print(",");
+
+  /* * * * * INDIVIDUAL PITCHES PRINTOUT * * * * */
+  for (int i = 1; i <= 3; i++) {
+    setActiveMpu(i);
+    // Serial.print(-IMU->madgwickFilter.getRoll() + 90); // prints pitch
+    Serial.print(IMU->madgwickFilter.getPitch()); // prints roll
+    Serial.print(",");
+  }
+
+  /* * * * * MEDIAN ROTATIONS PRINTOUTS * * * * */
+  // Serial.print(pitches[1]);
+  // Serial.print(","); 
+  Serial.print(rolls[1]);
+  // Serial.print(",");
+  // Serial.print(yaws[1]);
+  
+
+  /* * * * * ROTATION DIFFS PRINTOUTS * * * * */
+  // Serial.print("\t");
+  // Serial.print(pitches[0] - pitches[1]);
+  // Serial.print("\t/\t");
+  // Serial.print(pitches[2] - pitches[1]);
+  // Serial.print("\t\t\t");
+  // Serial.print(rolls[0] - rolls[1]);
+  // Serial.print("\t/\t");
+  // Serial.print(rolls[2] - rolls[1]);
+  // Serial.print("\t\t\t");
+  // Serial.print(yaws[0] - yaws[1]);
+  // Serial.print("\t/\t");
+  // Serial.println(yaws[2] - yaws[1]);
+
+
+  /* * * * * ACCELEROMETER ESTIMATION PRINTOUTS * * * * */
+  // Serial.print(IMU->accelAngle.x);
+  // Serial.print("\t");
+  // Serial.println(IMU->accelAngle.y);
+
+
+  /* * * * * GYROSCOPE ESTIMATION PRINTOUTS * * * * */
+  // Serial.print(IMU->gyroAngle.x);
+  // Serial.print("\t");
+  // Serial.print(IMU->gyroAngle.y);
+  // Serial.print("\t");
+  // Serial.println(IMU->gyroAngle.z);
+
+
+  /* * * * * DEVIATION STATUS PRINTOUTS * * * * */
+  Serial.print(",");
+  Serial.print(totalDeviations);
+
+
+  Serial.println("");
+}
+
+void playLoadingAnimation() {
+  sevenSegment.displayPChar(LOADING[loadingIndex / 8]);
+  loadingIndex = (loadingIndex + 1) % 160;
+}
---
+++ src/main.h
@@ -0,0 +1,171 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <MadgwickAHRS.h> // https://github.com/arduino-libraries/MadgwickAHRS/
+
+// Added for ESP-NOW communication
+#include <esp_now.h>
+#include <WiFi.h>
+
+#include "TM1637.h"
+
+// active MPU6050 address is 1101001, inactive is 1101000
+#define ADDRESS 0x69 // nice
+#define PIN_SCL 13 // SCL pin for MPU6050
+#define PIN_SDA 14 // SDA pin for MPU6050
+
+#define MPU1 25
+#define MPU2 33
+#define MPU3 32
+
+#define ACCEL_CONFIG 0x1C     // Accelerometer configuration address
+#define GYRO_CONFIG  0x1B     // Gyro configuration address
+
+#define ACCEL_SENSITIVITY 0 // 0 = 2g, 1 = 4g, 2 = 8g, 3 = 16g
+#define GYRO_SENSITIVITY 0 // 0 = 250rad/s, 1 = 500rad/s, 2 1000rad/s, 3 = 2000rad/s
+
+#define PWR_MGMT_1 0x6B
+#define PWR_MGMT_2 0x6C
+
+#define LOOP_TIME 10 // time in ms, (10 = 100Hz)
+#define MADGWICK_FREQ (1000 / LOOP_TIME)
+
+// a bit hacky but this slows down the printouts
+#define PRINTOUT_COOLDOWN 10 // 10 loops per printout
+
+#define RAD2DEG ( 180 / 3.1416 )
+
+
+// TM1637 stuff
+#define PIN_DIO 27 // DIO for TM1637 seven segment display
+#define PIN_CLK 26 // CLK for TM1637 seven segment display
+#define BLANK 11
+#define MINUS 10
+
+// status codes
+#define STATUS_OK 0x00
+#define STATUS_FINISHED_ALL_SETUP 0x01
+#define STATUS_FINISHED_PARTIAL_SETUP 0x02 // sent after each sub-device is setup successfully
+#define STATUS_SETUP_ERROR 0x03 // sent if partial setup fails
+#define STATUS_STARTING_COMMS_SETUP 0x04 // tbh this shouldn't even be sent???
+#define STATUS_STARTING_SSEG_SETUP 0x05
+#define STATUS_STARTING_I2C_SETUP 0x06
+#define STATUS_STARTING_MPU1_CALIBRATION 0x07
+#define STATUS_STARTING_MPU2_CALIBRATION 0x08
+#define STATUS_STARTING_MPU3_CALIBRATION 0x09
+#define STATUS_WARNING_HIGH_DEVIATION 0x0A // sent when one delta is high
+#define STATUS_WARNING_EXTREME_DEVIATION 0x0B // sent when BOTH deltas are high
+#define STATUS_CALC_ERROR 0x0C // sent if mpuCalc() fails
+#define STATUS_I2C_COMMS_ERROR 0x0D // sent if an I2C device fails to respond
+#define STATUS_SSEG_DISPLAY_ERROR 0x0E // sent if an error happens when trying to update the SSEG display
+#define STATUS_UNKNOWN_ERROR 0x0F // max value for an error code is 0x0F, the upper 4 bytes are unused
+
+#define DEVIATION_THRESHOLD 5 // number of degrees deviated required to trigger HIGH or EXTREME warnings
+
+long currentMs = 0; // current time im milliseconds, updated each time loop() is run
+long previousMs = 0; // last time loop() was run in milliseconds
+bool setupFinished = false;
+
+int printoutCounter = 0;
+
+float pitch; // Rotation around X axis
+float roll; // Rotation around Y axis
+float yaw; // Rotation around Z axis
+
+int totalDeviations; // used to count how many deviations are present in pitch & roll
+
+int yawOffset;
+
+int8_t HIDE_DECIMALS[6] = {0, 0, 0, 0, 0, 0};
+
+int loadingIndex = 0;
+char LOADING[20][20] = {
+  "=     ",
+  "==    ",
+  "===   ",
+  "====  ",
+  " ==== ",
+  "  ====",
+  "   ===",
+  "    ==",
+  "     =",
+  "     =",
+  "     =",
+  "    ==",
+  "   ===",
+  "  ====",
+  " ==== ",
+  "====  ",
+  "===   ",
+  "==    ",
+  "=     ",
+  "=     "
+};
+
+
+void sendPacket(int, int, int);
+void sendStatusUpdate(int);
+void espNowSetup(void);
+void mpuSetup(int);
+void mpuCalc(void);
+void transmit(char, bool);
+void transmit(char, char, bool);
+void setActiveMpu(int);
+float vectorToAngle(float, float, float);
+void sortFloats(float*);
+void updateDisplay(int, int);
+int countDeviation(float);
+void playLoadingAnimation(void);
+void printData(void);
+
+// ESP32-C3 receiver address (64:e8:33:84:92:6c)
+uint8_t receiverAddress[] = {0x64,0xe8,0x33,0x84,0x92,0x6c};
+
+class XYZ {
+  public:
+    float x;
+    float y;
+    float z;
+};
+
+class XYZint {
+  public:
+    int16_t x;
+    int16_t y;
+    int16_t z;
+};
+
+class MpuData {
+  public:
+    XYZ accel;
+    XYZ accelOffset;
+    XYZ gyro;
+    XYZint gyroRaw;
+    XYZint gyroOffset;
+    XYZ accelAngle; // X = pitch, Y = roll, no Z
+    XYZ gyroAngle; // X = pitch, Y = roll, no Z
+
+    Madgwick madgwickFilter;
+};
+
+// packet class to be sent over ESP-NOW
+// technically a "struct" since I don't know if classes can be sent
+typedef struct rotationPacket {
+    char yaw; // -180 -> 180
+    char pitch; // -90 -> 90
+    char roll; // -90 -> 90
+    char status; // see status codes above
+} rotationPacket;
+
+MpuData* IMU; // Active MPU, assigned using setActiveMpu()
+MpuData mpu1; // MPU6050 unit 1
+MpuData mpu2; // MPU6050 unit 2
+MpuData mpu3; // MPU6050 unit 3
+
+TM1637 sevenSegment;
+
+float pitches[3] = {0, 0, 0};
+float rolls[3] = {0, 0, 0};
+float yaws[3] = {0, 0, 0};
+
+rotationPacket packet;
+esp_now_peer_info_t espReceiver;
